1fbc5afdbab7253551170baca2136a8b97015064
Manuel Strehl <git@manuel-strehl.de>
2015-04-30 23:36:00 +0200

    create a JSON file for current post

--- Makefile
+++ Makefile
@@ -2,8 +2,11 @@ POSTS := $(patsubst posts/%.html,%.html,$(wildcard posts/*.html))
 
 all: $(POSTS) index.html
 
-index.html: $(POSTS)
+index.html: $(POSTS) current.json
 	@compile.js $@
 
 $(POSTS): %.html : posts/%.html
 	@compile.js $<
+
+current.json: content.json
+	@create_fragment.js
---
+++ create_fragment.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+
+var content = JSON.parse(fs.readFileSync('./content.json', {encoding:'utf-8'}));
+
+var data = fs.readFileSync('./posts/'+content.posts[0].slug+'.html', {encoding:'utf-8'});
+data = sanitizeHtml(data, {
+  allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
+}).substr(0, 255);
+
+fs.writeFileSync('./current.json', JSON.stringify({
+  content: data,
+  url: 'https://blog.codepoints.net/'+content.posts[0].slug+'.html',
+}));
